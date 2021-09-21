7aadbe487b8277f1b0aa753b126c04e5c8bdef44
WillMB2003 <90205024+WillMB2003@users.noreply.github.com>
2021-09-21 10:58:45 +0100

    Update app.js
    Removed unnecessary code.

--- app.js
+++ app.js
@@ -4,14 +4,4 @@ const Post = require("./post");
 const Comment = require("./comment");
 const Poll = require("./poll")
 
-const Politics = new Forum("Politics");
-const Sarah = new User("Sar22");
-const Eddy = new User("Edd");
-const mrAdmin = new User("AdminMan", true)
-const globalWarming = Sarah.createPost(Politics,
-    "Is global warming real?", "Can someone tell me?");
-Eddy.createComment(Politics, "Is global warming real?", "Nah can't be!");
-Eddy.createComment(Politics, "Is global warming real?", "Stupid question!");
-console.log(globalWarming.comments)
-mrAdmin.deleteComment(Politics, "Is global warming real?", "Stupid question!")
-console.log(globalWarming.comments)
+// Code here
